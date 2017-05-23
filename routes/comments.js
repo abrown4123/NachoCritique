d00e8f0b3188a51d54fc69606e746256e904fa97
abrown4123 <abrown4123@gmail.com>
2017-05-23 03:06:44 +0000

    Added the ability to edit comments

--- routes/comments.js
+++ routes/comments.js
@@ -41,6 +41,28 @@ router.post("/", isLoggedIn, function(req, res){
     });
 });
 
+//Edit comment route
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        } else{
+            res.render("comments/edit", {nacho_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+//Comment Update
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, newComment){
+        if(err){
+            res.redirect("back");
+        } else{
+            res.redirect("/nachos/" + req.params.id);
+        }
+    });
+});
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
